0ab8414ce13b8856b2a507bcaa2a5780d721e46f
TatsianaSauko <miljanzowa@mail.ru>
2024-10-19 15:39:46 +0300

    feat: change middlewaarw

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,4 +1,8 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+// Загрузка переменных окружения из .env файла
+dotenv.config();
 
 module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
@@ -14,10 +18,11 @@ module.exports = (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, 'your-secret-key');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Использование переменной окружения
         req.user = decoded;
         next();
     } catch (err) {
+        console.error('Ошибка проверки токена:', err);
         return res.status(401).json({
             statusCode: 401,
             error: 'Unauthorized',
@@ -25,4 +30,3 @@ module.exports = (req, res, next) => {
         });
     }
 };
-
