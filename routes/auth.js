e95a1f625c158cc39e5851c2105ca11bb5c25a11
TatsianaSauko <miljanzowa@mail.ru>
2024-10-19 15:35:42 +0300

    feat: add auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+// Загрузка переменных окружения из .env файла
+dotenv.config();
 
 // Маршрут для регистрации
 router.post('/registration', async (req, res) => {
@@ -57,64 +61,13 @@ router.post('/login', async (req, res) => {
             });
         }
 
-        const accessToken = jwt.sign({ id: user._id }, 'your-secret-key', { expiresIn: '1h' });
+        const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         res.status(200).json({
             accessToken
         });
     } catch (error) {
         console.error('Ошибка авторизации:', error);
-        res.status(500).json({
-            statusCode: 500,
-            error: 'Internal Server Error',
-            message: 'Ошибка сервера'
-        });
-    }
-});
-
-// Маршрут для изменения пароля
-router.post('/change-password', async (req, res) => {
-    const { password, confirmPassword } = req.body;
-    const emailVerified = req.cookies.emailVerified;
-    const userEmail = req.cookies.userEmail;
-
-    if (!emailVerified) {
-        return res.status(400).json({
-            statusCode: 400,
-            error: 'Bad Request',
-            message: 'Email не подтвержден'
-        });
-    }
-
-    if (password !== confirmPassword) {
-        return res.status(400).json({
-            statusCode: 400,
-            error: 'Bad Request',
-            message: 'Пароли не совпадают'
-        });
-    }
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.findOneAndUpdate(
-            { email: userEmail },
-            { password: hashedPassword },
-            { new: true }
-        );
-
-        if (!user) {
-            return res.status(404).json({
-                statusCode: 404,
-                error: 'Not Found',
-                message: 'Пользователь не найден'
-            });
-        }
-
-        res.status(201).json({
-            message: 'Пароль успешно изменен'
-        });
-    } catch (error) {
-        console.error('Ошибка изменения пароля:', error);
         res.status(500).json({
             statusCode: 500,
             error: 'Internal Server Error',
