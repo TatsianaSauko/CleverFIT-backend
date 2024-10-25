9b0acaa00d1c862e2492930d2a8a03967f411368
AlexMakovetskiy <bo5634302@gmail.com>
2024-10-25 14:51:15 +0300

    init: supplement app configuration

---
+++ README.md
@@ -0,0 +1,8 @@
+# CleverFIT API Test
+
+Express.js API
+
+Environment vars:
+
+MONGO_URI=
+JWT_SECRET=
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+dotenv.config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
